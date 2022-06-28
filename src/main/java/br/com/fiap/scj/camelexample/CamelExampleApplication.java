10782463189b060554c51caabdb4288b3cada67e
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-06-28 19:46:18 -0300

    finalizacao camel

--- src/main/java/br/com/fiap/scj/camelexample/CamelExampleApplication.java
+++ src/main/java/br/com/fiap/scj/camelexample/CamelExampleApplication.java
@@ -1,6 +1,10 @@
 package br.com.fiap.scj.camelexample;
 
+import br.com.fiap.scj.camelexample.beans.MyObject;
+import br.com.fiap.scj.camelexample.services.ObjectService;
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -12,6 +16,8 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
+
+import javax.ws.rs.core.MediaType;
 
 @SpringBootApplication
 public class CamelExampleApplication {
@@ -55,7 +61,28 @@ public class CamelExampleApplication {
 
 			rest("/api")
 					.description("Test Camel Service")
-					.id("api-route");
+					.id("api-route")
+					.post("/test")
+					.produces(MediaType.APPLICATION_JSON)
+					.consumes(MediaType.APPLICATION_JSON)
+					.bindingMode(RestBindingMode.auto)
+					.type(MyObject.class)
+					.enableCORS(true)
+					.to("direct:remoteService");
+
+			from("direct:remoteService")
+					.routeId("direct-route")
+					.log(">>> ${body.id}")
+					.log(">>> ${body.name}")
+					.process(new Processor() {
+						@Override
+						public void process(Exchange exchange) throws Exception {
+							MyObject obj = (MyObject) exchange.getIn().getBody();
+							ObjectService.execute(obj);
+							exchange.getIn().setBody(obj);
+						}
+					})
+					.setHeader(Exchange.HTTP_RESPONSE_CODE, constant(201));
 		}
 	}
 
